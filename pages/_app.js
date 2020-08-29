1c08426b0094f59a7212818edd859b42513e9692
Pierre <you@example.com>
2020-08-29 11:18:55 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../src/css/main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
