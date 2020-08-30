6f52b86d38d4a05d5f8dd7742d5675c879a91f95
Pierre <you@example.com>
2020-08-30 17:31:25 +0200

    mobile fixed

--- components/SectionBody.jsx
+++ components/SectionBody.jsx
@@ -4,27 +4,49 @@ export default function SectionBody(props) {
   return (
     <>
       {props.bg ? (
-        <section
-          className="section py-0"
-          style={{
-            background: `transparent url(${props.bg}) center center no-repeat`,
-            backgroundSize: "cover",
-            backgroundAttachment: "fixed",
-          }}
-        >
-          <div
+        <>
+          <section
+            className="section py-0 is-hidden-mobile"
             style={{
-              backgroundColor: "rgba(47, 54, 64, 0.6)",
-              height: "100%",
+              background: `transparent url(${props.bg}) center center no-repeat`,
+              backgroundSize: "cover",
+              backgroundAttachment: "fixed",
             }}
           >
-            <div className="container py-6">
-              <div className="content has-text-centered has-text-white">
-                {props.children}
+            <div
+              style={{
+                backgroundColor: "rgba(47, 54, 64, 0.6)",
+                height: "100%",
+              }}
+            >
+              <div className="container py-6">
+                <div className="content has-text-centered has-text-white">
+                  {props.children}
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section
+            className="section py-0 is-hidden-tablet"
+            style={{
+              background: `transparent url(${props.bg}) center center no-repeat`,
+              backgroundSize: "cover",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "rgba(47, 54, 64, 0.6)",
+                height: "100%",
+              }}
+            >
+              <div className="container py-6">
+                <div className="content has-text-centered has-text-white">
+                  {props.children}
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
       ) : (
         <section className="section my-2">
           <div className="container">
