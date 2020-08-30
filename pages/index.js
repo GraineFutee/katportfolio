4cb9365435118caeeb9e303fa605b1ee9208b890
Pierre <you@example.com>
2020-08-30 17:25:54 +0200

    mobile fixed

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Website() {
       <Head>
         <title>StrandbergLegal AB</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0"
+        ></meta>
       </Head>
 
       <main>
